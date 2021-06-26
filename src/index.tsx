74c19f726f0b2c389e7d09b4216c946df0689cdd
danil.sechin <reymonsma@gmail.com>
2021-06-26 22:36:14 +0500

    Add react router dom

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { HashRouter, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <HashRouter>
+        <Route exact path="/" render={() => <App />} />
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
